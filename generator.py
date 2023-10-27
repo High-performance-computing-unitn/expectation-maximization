f1df81a8a259c5ff24ff63881afdf78a75efb581
Davide Moletta <davide.moletta@studenti.unitn.it>
2023-10-27 14:53:47 +0200

    generator now working for multivariate gaussian distribution

--- generator.py
+++ generator.py
@@ -1,67 +1,44 @@
 from numpy import random
-import matplotlib.pyplot as plt
-
-f = open("data.txt", "a")
-
-def randomFiller(dimensions):
-    mu = [0] * dimensions #Mean of the N-dimensional distribution of length N
-    cov = [[0 for _ in range(dimensions)] for _ in range(dimensions)] #Covariance matrix of the distribution. It must be symmetric and positive-semidefinite for proper sampling. Size NxN
-   
-    for i in range(dimensions):
-        mu[i] = (round(random.uniform(0, 1000), 2))
-        for j in range(dimensions):
-            cov[i][j] = mu[i]/10
-
-    return (mu, cov)
+import numpy as np
+import pandas as pd
 
 def gaussianGenerator(gaussiansNumber, dimensions):
     gaussians = []
 
     while(len(gaussians) < gaussiansNumber):
 
-        (mu, cov) = randomFiller(dimensions)
+        mu = np.random.randint(-1e5, 1e5, dimensions)
+        A = np.random.rand(dimensions, dimensions)
+        cov = np.dot(A, A.transpose())
 
-        gaussian = {"mu": mu, "cov": cov}
-        gaussians.append(gaussian)
+        gaussians.append({"mu": mu, "cov": cov})
 
-    #print(gaussians)
     return gaussians
-
-
-def pointGenerator(dimensions, mu, cov):
-    points = random.multivariate_normal(mu, cov, (dimensions, dimensions, dimensions))
-    #points = random.normal(mu, sigma, (dataset["samples"], dataset["dimensions"]))
-    for point in points:
-        for p in point:
-            f.write(str(point) + " ")
-        f.write("\n")
-    return points
 
 def main():
 
     print("How many gaussians? ")
-    gaussiansNumber = input()
+    gaussiansNumber = int(input())
 
     print("How many dimensions? ")
-    dimensions = input()
+    dimensions = int(input())
 
-    gaussians = gaussianGenerator(int(gaussiansNumber), int(dimensions))
+    gaussians = gaussianGenerator(gaussiansNumber, dimensions)
 
     print("Which dataset? (s // m // l)")
     datasetSize = input()
 
-    for gaussian in gaussians:
-        pointGenerator(int(dimensions), gaussian["mu"], gaussian["cov"])
+    match datasetSize:
+        case "s":
+            samples = 200
+        case "m":
+            samples = 10000
+        case "l":
+            samples = 5000000
+        case _:
+            print("Invalid value")   
+
+    for gaussian in gaussians:        
+        pd.DataFrame(random.multivariate_normal( gaussian["mu"], gaussian["cov"], samples)).to_csv("data.csv", index=False, header=False, mode="a")
 
 main()
-f.close()
-
-"""   match datasetSize:
-        case "s":
-            dataset = {"samples": 200, "dimensions": 1}
-        case "m":
-            dataset = {"samples": 2000, "dimensions": 10}
-        case "l":
-            dataset = {"samples": 1000000, "dimensions": 100}
-        case _:
-            print("Invalid value")     """
