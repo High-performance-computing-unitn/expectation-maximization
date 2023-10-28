6793168017acb888e870e387430a30b99e577b0c
Davide Moletta <davide.moletta@studenti.unitn.it>
2023-10-28 14:48:08 +0200

    Added gaussians data file for future comparison. Minor fix

--- Data generator/generator.py
+++ Data generator/generator.py
@@ -2,6 +2,8 @@ from numpy import random
 import numpy as np
 import pandas as pd
 
+f = open("Data generator/GaussiansInformations.txt", "a")
+
 def gaussianGenerator(gaussiansNumber, dimensions):
     gaussians = []
 
@@ -10,6 +12,8 @@ def gaussianGenerator(gaussiansNumber, dimensions):
         mu = np.random.randint(-1e5, 1e5, dimensions)
         A = np.random.rand(dimensions, dimensions)
         cov = np.dot(A, A.transpose())
+
+        f.write("new gaussian \n Mu: " + str(mu) + "\n Cov: " + str(cov) + "\n\n")
 
         gaussians.append({"mu": mu, "cov": cov})
 
@@ -31,14 +35,17 @@ def main():
     match datasetSize:
         case "s":
             samples = 200
+            path = "Data generator/smallDataset.csv"
         case "m":
             samples = 10000
+            path = "Data generator/mediumDataset.csv"
         case "l":
             samples = 5000000
+            path = "Data generator/bigDataset.csv"
         case _:
             print("Invalid value")   
 
     for gaussian in gaussians:        
-        pd.DataFrame(random.multivariate_normal( gaussian["mu"], gaussian["cov"], samples)).to_csv("data.csv", index=False, header=False, mode="a")
+        pd.DataFrame(random.multivariate_normal( gaussian["mu"], gaussian["cov"], samples)).to_csv(path, index=False, header=False, mode="a")
 
 main()
