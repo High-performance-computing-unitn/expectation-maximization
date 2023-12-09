03a9b24af5ecaf4686a267ad24a1c085f4ae2f2a
Moletta <davide.moletta@hpc-head-n1.unitn.it>
2023-12-09 12:06:36 +0100

    Added serial version results

--- serial-version/serialRunBig.sh
+++ serial-version/serialRunBig.sh
@@ -7,4 +7,4 @@
 #PBS -q short_cpuQ
 
 # arguments are                                     N       D K iter filepath
-./expectation-maximization/serial-version/serialRun 1250000 3 5 200 "expectation-maximization/data-generator/N1250000_K5_D3.csv"
+./expectation-maximization/serial-version/serialRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
--- serial-version/serialRunMedium.sh
+++ serial-version/serialRunMedium.sh
@@ -7,4 +7,4 @@
 #PBS -q short_cpuQ
 
 # arguments are                                     N      D K iter filepath
-./expectation-maximization/serial-version/serialRun 500000 3 5 200 "expectation-maximization/data-generator/N500000_K5_D3.csv"
+./expectation-maximization/serial-version/serialRun 625000 4 5 200 "expectation-maximization/data-generator/N625000_K5_D4.csv"
