8986f43de793063eef13656f0f9ad1dc7a055f9d
Moletta <davide.moletta@hpc-head-n2.unitn.it>
2023-12-11 09:42:52 +0100

    added last results

--- parallel-second-implementation/parallelRunBig.sh
+++ parallel-second-implementation/parallelRunBig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=2:mem=1gb
+#PBS -l select=1:ncpus=64:mem=1gb
 
-#PBS -l walltime=00:50:00
+#PBS -l walltime=00:10:00
 
 #PBS -q short_cpuQ
 
@@ -10,42 +10,42 @@ module load mpich-3.2
 
 echo "execution with 2 cores"
 
-# arguments are                     executable                                  N       D K iter filepath
+# arguments are                     executable                                   N       D K iter filepath
 mpiexec -n 2 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
 echo "---end---"
 
-# echo "execution with 4 cores"
+echo "execution with 4 cores"
 
-# # arguments are                     executable                                  N   D K iter filepath
-# mpiexec -n 4 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
+# arguments are                     executable                                   N       D K iter filepath
+mpiexec -n 4 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
-# echo "---end---"
+echo "---end---"
 
-# echo "execution with 8 cores"
+echo "execution with 8 cores"
 
-# # arguments are                     executable                                  N   D K iter filepath
-# mpiexec -n 8 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
+# arguments are                     executable                                   N       D K iter filepath
+mpiexec -n 8 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
-# echo "---end---"
+echo "---end---"
 
-# echo "execution with 16 cores"
+echo "execution with 16 cores"
 
-# # arguments are                     executable                                  N   D K iter filepath
-# mpiexec -n 16 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
+# arguments are                     executable                                    N       D K iter filepath
+mpiexec -n 16 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
-# echo "---end---"
+echo "---end---"
 
-# echo "execution with 32 cores"
+echo "execution with 32 cores"
 
-# # arguments are                     executable                                  N   D K iter filepath
-# mpiexec -n 32 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
+# arguments are                     executable                                    N       D K iter filepath
+mpiexec -n 32 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
-# echo "---end---"
+echo "---end---"
 
-# echo "execution with 64 cores"
+echo "execution with 64 cores"
 
-# # arguments are                     executable                                  N   D K iter filepath
-# mpiexec -n 64 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
+# arguments are                     executable                                    N       D K iter filepath
+mpiexec -n 64 expectation-maximization/parallel-second-implementation/parallelRun 1250000 4 5 200 "expectation-maximization/data-generator/N1250000_K5_D4.csv"
 
-# echo "---end---"
+echo "---end---"
