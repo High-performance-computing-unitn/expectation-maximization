ded4c7cd3fce5f906f06ebba9723e1c073365612
Moletta <davide.moletta@hpc-head-n2.unitn.it>
2023-12-11 15:22:24 +0100

    addded high dimensions results

--- data-generator/plotter.py
+++ data-generator/plotter.py
@@ -21,6 +21,12 @@ parallel_2_algo_MDS = []
 parallel_2_file_BDS = []
 parallel_2_algo_BDS = []
 
+parallel_1_file_HD = [0.08, 0.3, 0.03]
+parallel_1_algo_HD = [304, 166, 83]
+
+parallel_2_file_HD = [0.02, 0.01, 0.007]
+parallel_2_algo_HD = [17, 15, 12]
+
 version = 0
 dataset = 0
 
@@ -76,6 +82,7 @@ with open('../results_formatted.txt') as f:
 
 x_ticks = [1, 2, 4, 8, 16, 32, 64]
 x_values = [2, 4, 8, 16, 32, 64]
+x_ticks_HD[2, 4, 8]
 
 # SMALL DATASET PLOT
 
@@ -178,6 +185,39 @@ algo.set_xticks(range(len(x_ticks)), x_ticks)
 algo.set_xlim(-0.5, len(x_ticks) - 0.5)
 
 algo.set_title('Big dataset algorithm time') 
+algo.legend()
+  
+plt.show() 
+
+# HIGH DIMENSIONAL DATASET PLOT
+
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (file, algo) = plt.subplots(2, 1)
+
+file.plot(range(len(x_ticks_HD)), parallel_1_file_HD, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+file.plot(range(len(x_ticks_HD)), parallel_2_file_HD, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+file.set_xlabel('processes') 
+file.set_ylabel('seconds') 
+
+file.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
+file.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
+
+file.set_title('High dimensional dataset file reading time') 
+file.legend()
+
+algo.plot(range(len(x_ticks_HD)), parallel_1_algo_HD, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+algo.plot(range(len(x_ticks_HD)), parallel_2_algo_HD, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+algo.set_xlabel('processes') 
+algo.set_ylabel('seconds') 
+
+algo.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
+algo.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
+
+algo.set_title('High dimensional dataset algorithm time') 
 algo.legend()
   
 plt.show() 
