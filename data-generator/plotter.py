0686731777b9c50132482ea84bbf6a3633a97e8c
davide-moletta <davide.moletta@studenti.unitn.it>
2023-12-27 16:22:42 +0100

    updated plotter and added new datasets

--- data-generator/plotter.py
+++ data-generator/plotter.py
@@ -21,11 +21,17 @@ parallel_2_algo_MDS = []
 parallel_2_file_BDS = []
 parallel_2_algo_BDS = []
 
-parallel_1_file_HD = [0.08, 0.03, 0.03, 0.15, 0.03, 0.14]
-parallel_1_algo_HD = [304, 166, 83, 42, 20, 19]
+parallel_1_file_6D = [0.08, 0.03, 0.03, 0.15, 0.03, 0.14]
+parallel_1_algo_6D = [304, 166, 83, 42, 20, 19]
 
-parallel_2_file_HD = [0.02, 0.01, 0.007, 0.01, 0.02, 0.01]
-parallel_2_algo_HD = [17, 15, 12, 7, 4, 8]
+parallel_2_file_6D = [0.02, 0.01, 0.007, 0.01, 0.02, 0.01]
+parallel_2_algo_6D = [17, 15, 12, 7, 4, 8]
+
+parallel_1_file_8D = [0.03, 0.02, 0.01, 0.3, 0.008, 0.008]
+parallel_1_algo_8D = [5177, 2537, 1476, 705, 415, 114]
+
+parallel_2_file_8D = [0.006, 0.003, 0.005, 0.005, 0.02, 0.006]
+parallel_2_algo_8D = [4.6, 4.4, 2.3, 2.4, 0.7, 1]
 
 version = 0
 dataset = 0
@@ -188,15 +194,15 @@ algo.legend()
   
 plt.show() 
 
-# HIGH DIMENSIONAL DATASET PLOT
+# 6 DIMENSIONAL DATASET PLOT
 
 plt.rcParams["figure.figsize"] = [8, 8]
 plt.rcParams["figure.autolayout"] = True
 
 fig, (file, algo) = plt.subplots(2, 1)
 
-file.plot(range(len(x_ticks_HD)), parallel_1_file_HD, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
-file.plot(range(len(x_ticks_HD)), parallel_2_file_HD, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+file.plot(range(len(x_ticks_HD)), parallel_1_file_6D, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+file.plot(range(len(x_ticks_HD)), parallel_2_file_6D, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
 
 file.set_xlabel('processes') 
 file.set_ylabel('seconds') 
@@ -204,11 +210,11 @@ file.set_ylabel('seconds')
 file.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
 file.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
 
-file.set_title('High dimensional dataset file reading time') 
+file.set_title('Six dimensional dataset file reading time') 
 file.legend()
 
-algo.plot(range(len(x_ticks_HD)), parallel_1_algo_HD, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
-algo.plot(range(len(x_ticks_HD)), parallel_2_algo_HD, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+algo.plot(range(len(x_ticks_HD)), parallel_1_algo_6D, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+algo.plot(range(len(x_ticks_HD)), parallel_2_algo_6D, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
 
 algo.set_xlabel('processes') 
 algo.set_ylabel('seconds') 
@@ -216,7 +222,202 @@ algo.set_ylabel('seconds')
 algo.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
 algo.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
 
-algo.set_title('High dimensional dataset algorithm time') 
+algo.set_title('Six dimensional dataset algorithm time') 
 algo.legend()
+  
+plt.show() 
+
+# 8 DIMENSIONAL DATASET PLOT
+
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (file, algo) = plt.subplots(2, 1)
+
+file.plot(range(len(x_ticks_HD)), parallel_1_file_8D, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+file.plot(range(len(x_ticks_HD)), parallel_2_file_8D, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+file.set_xlabel('processes') 
+file.set_ylabel('seconds') 
+
+file.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
+file.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
+
+file.set_title('Eight dimensional dataset file reading time') 
+file.legend()
+
+algo.plot(range(len(x_ticks_HD)), parallel_1_algo_8D, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+algo.plot(range(len(x_ticks_HD)), parallel_2_algo_8D, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+algo.set_xlabel('processes') 
+algo.set_ylabel('seconds') 
+
+algo.set_xticks(range(len(x_ticks_HD)), x_ticks_HD)
+algo.set_xlim(-0.5, len(x_ticks_HD) - 0.5)
+
+algo.set_title('Eight dimensional dataset algorithm time') 
+algo.legend()
+  
+plt.show() 
+
+
+## EVALUATION SPEEDUP AND EFFICIENCY
+
+parallel_1_algo_Speedup_SDS = []
+parallel_1_algo_Efficiency_SDS = []
+
+parallel_1_algo_Speedup_MDS = []
+parallel_1_algo_Efficiency_MDS = []
+
+parallel_1_algo_Speedup_BDS = []
+parallel_1_algo_Efficiency_BDS = []
+
+parallel_2_algo_Speedup_SDS = []
+parallel_2_algo_Efficiency_SDS = []
+
+parallel_2_algo_Speedup_MDS = []
+parallel_2_algo_Efficiency_MDS = []
+
+parallel_2_algo_Speedup_BDS = []
+parallel_2_algo_Efficiency_BDS = []
+
+for index, time in enumerate(parallel_1_algo_SDS):
+    speedup = parallel_1_algo_SDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_1_algo_Speedup_SDS.append(speedup)
+    parallel_1_algo_Efficiency_SDS.append(efficiency)
+
+for index, time in enumerate(parallel_1_algo_MDS):
+    speedup = parallel_1_algo_MDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_1_algo_Speedup_MDS.append(speedup)
+    parallel_1_algo_Efficiency_MDS.append(efficiency)
+
+for index, time in enumerate(parallel_1_algo_BDS):
+    speedup = parallel_1_algo_BDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_1_algo_Speedup_BDS.append(speedup)
+    parallel_1_algo_Efficiency_BDS.append(efficiency)
+
+for index, time in enumerate(parallel_2_algo_SDS):
+    speedup = parallel_2_algo_SDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_2_algo_Speedup_SDS.append(speedup)
+    parallel_2_algo_Efficiency_SDS.append(efficiency)
+
+for index, time in enumerate(parallel_2_algo_MDS):
+    speedup = parallel_2_algo_MDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_2_algo_Speedup_MDS.append(speedup)
+    parallel_2_algo_Efficiency_MDS.append(efficiency)
+
+for index, time in enumerate(parallel_2_algo_BDS):
+    speedup = parallel_2_algo_BDS[0] / time
+    efficiency = speedup / x_values[index]
+
+    parallel_2_algo_Speedup_BDS.append(speedup)
+    parallel_2_algo_Efficiency_BDS.append(efficiency)
+
+# SMALL DATASET SPEEDUP AND EFFICIENCY
+    
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (speedup, efficiency) = plt.subplots(2, 1)
+
+speedup.plot(range(0, len(x_values)), parallel_1_algo_Speedup_SDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+speedup.plot(range(0, len(x_values)), parallel_2_algo_Speedup_SDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+speedup.set_xlabel('processes') 
+speedup.set_ylabel('speedup') 
+
+speedup.set_xticks(range(len(x_values)), x_values)
+speedup.set_xlim(-0.5, len(x_values) - 0.5)
+
+speedup.set_title('Small dataset speedup comparison') 
+speedup.legend()
+
+efficiency.plot(range(0, len(x_values)), parallel_1_algo_Efficiency_SDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+efficiency.plot(range(0, len(x_values)), parallel_2_algo_Efficiency_SDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+efficiency.set_xlabel('processes') 
+efficiency.set_ylabel('effciency') 
+
+efficiency.set_xticks(range(len(x_values)), x_values)
+efficiency.set_xlim(-0.5, len(x_values) - 0.5)
+
+efficiency.set_title('Small dataset efficiency comaprison') 
+efficiency.legend()
+  
+plt.show() 
+
+# MEDIUM DATASET SPEEDUP AND EFFICIENCY
+    
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (speedup, efficiency) = plt.subplots(2, 1)
+
+speedup.plot(range(0, len(x_values)), parallel_1_algo_Speedup_MDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+speedup.plot(range(0, len(x_values)), parallel_2_algo_Speedup_MDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+speedup.set_xlabel('processes') 
+speedup.set_ylabel('speedup') 
+
+speedup.set_xticks(range(len(x_values)), x_values)
+speedup.set_xlim(-0.5, len(x_values) - 0.5)
+
+speedup.set_title('Medium dataset speedup comparison') 
+speedup.legend()
+
+efficiency.plot(range(0, len(x_values)), parallel_1_algo_Efficiency_MDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+efficiency.plot(range(0, len(x_values)), parallel_2_algo_Efficiency_MDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+efficiency.set_xlabel('processes') 
+efficiency.set_ylabel('effciency') 
+
+efficiency.set_xticks(range(len(x_values)), x_values)
+efficiency.set_xlim(-0.5, len(x_values) - 0.5)
+
+efficiency.set_title('Medium dataset efficiency comaprison') 
+efficiency.legend()
+  
+plt.show() 
+
+# BIG DATASET SPEEDUP AND EFFICIENCY
+    
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+
+fig, (speedup, efficiency) = plt.subplots(2, 1)
+
+speedup.plot(range(0, len(x_values)), parallel_1_algo_Speedup_BDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+speedup.plot(range(0, len(x_values)), parallel_2_algo_Speedup_BDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+speedup.set_xlabel('processes') 
+speedup.set_ylabel('speedup') 
+
+speedup.set_xticks(range(len(x_values)), x_values)
+speedup.set_xlim(-0.5, len(x_values) - 0.5)
+
+speedup.set_title('Big dataset speedup comparison') 
+speedup.legend()
+
+efficiency.plot(range(0, len(x_values)), parallel_1_algo_Efficiency_BDS, label = "parallel 1", color = 'blue', linewidth = 2, marker='o', markerfacecolor='blue', markersize=5)
+efficiency.plot(range(0, len(x_values)), parallel_2_algo_Efficiency_BDS, label = "parallel 2", color = 'red', linewidth = 2, marker='o', markerfacecolor='red', markersize=5)
+
+efficiency.set_xlabel('processes') 
+efficiency.set_ylabel('effciency') 
+
+efficiency.set_xticks(range(len(x_values)), x_values)
+efficiency.set_xlim(-0.5, len(x_values) - 0.5)
+
+efficiency.set_title('Big dataset efficiency comaprison') 
+efficiency.legend()
   
 plt.show() 
